e878a21b35cc8000f5e8854fe5dd2be0666930cf
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-10-23 23:16:59 +0100

    fix: Support migration runner path on Windows

--- migrator/reader.go
+++ migrator/reader.go
@@ -20,6 +20,8 @@ type MigrationReader struct {
 }
 
 func NewMigrationReader(migrationLocation string) *MigrationReader {
+	migrationLocation = normalizePath(migrationLocation)
+
 	return &MigrationReader{
 		MigrationLocation: &migrationLocation,
 	}
@@ -27,6 +29,10 @@ func NewMigrationReader(migrationLocation string) *MigrationReader {
 
 func NewMigrationReaderStatic() *MigrationReader {
 	return &MigrationReader{}
+}
+
+func normalizePath(path string) string {
+	return strings.ReplaceAll(path, "/", string(os.PathSeparator))
 }
 
 func migrationIndexFromName(name string) (int, error) {
@@ -53,6 +59,8 @@ func (m *MigrationReader) sortMigrationsList(migrations []Migration) []Migration
 
 func (m *MigrationReader) ListAllMigrations(migrationRootFolder string) []Migration {
 	var migrations []Migration
+
+	migrationRootFolder = normalizePath(migrationRootFolder)
 
 	err := filepath.Walk(migrationRootFolder, func(path string, info os.FileInfo, err error) error {
 		if path != migrationRootFolder {
